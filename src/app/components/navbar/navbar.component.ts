13bee9ac3e27dba5a24e5b2101ce0b3ae2e935e0
jaumebarber <jaumebarber@gmail.com>
2019-07-30 10:47:03 +0200

    added navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { Input } from '@angular/core';
-
 
 @Component({
   selector: 'app-navbar',
   templateUrl: 'navbar.component.html',
   styleUrls: ['navbar.component.css'],
 })
-export class NavbarComponent {
-  @Input() title: string;
-}
-
+export class NavbarComponent {}
