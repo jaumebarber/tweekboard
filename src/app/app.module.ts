1ac1a07ba5eaaf323ac875c6dba51e7534419491
jaumebarber <jaumebarber@gmail.com>
2019-07-30 10:47:03 +0200

    added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule,
-    MatCardModule
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
