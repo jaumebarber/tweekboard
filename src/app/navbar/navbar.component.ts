0cc96a74b356977b66cebdd77b6a340a520a1cf7
jaumebarber <jaumebarber@gmail.com>
2019-07-31 15:23:43 +0200

    added toolbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,21 +1,11 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, share } from 'rxjs/operators';
+import {Component} from '@angular/core';
 
+/**
+ * @title Multi-row toolbar
+ */
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  templateUrl: 'navbar.component.html',
+  styleUrls: ['navbar.component.css'],
 })
-export class NavbarComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      share()
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
-}
+export class NavbarComponent {}
