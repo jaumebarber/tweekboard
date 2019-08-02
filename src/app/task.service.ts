a231d820dad06173ae69dc2567f0ec71eaa53a03
jaumebarber <jaumebarber@gmail.com>
2019-08-02 14:39:18 +0200

    implemented task delete

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Task } from './task';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class TaskService {
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   private tasksUrl = 'api/tasks';
   tasks: Task[];
@@ -17,5 +20,17 @@ export class TaskService {
 
   getTasks():Observable<Task[]> {
     return this.http.get<Task[]>(this.tasksUrl);
+  }
+
+  getTask(id: number): Observable<Task> {
+    const url = `${this.tasksUrl}/${id}`;
+    return this.http.get<Task>(url)
+  }
+
+  deleteTask (task: Task | number): Observable<Task> {
+    const id = typeof task === 'number' ? task : task.id;
+    const url = `${this.tasksUrl}/${id}`;
+  
+    return this.http.delete<Task>(url, this.httpOptions)
   }
 }
