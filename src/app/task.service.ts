14b1fd6f0ed53ca9831fe69993a13c51b54f0a44
Jaume Barber <jaume@hipervincle.com>
2019-08-05 10:20:20 +0200

    addTask and updateTask functionalities implemented

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Task } from './task';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,21 +18,56 @@ export class TaskService {
   private tasksUrl = 'api/tasks';
   tasks: Task[];
 
-  constructor( private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private messageSrv: MessageService
+  ) { }
 
-  getTasks():Observable<Task[]> {
-    return this.http.get<Task[]>(this.tasksUrl);
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.tasksUrl, task, this.httpOptions).pipe(
+      tap((newTask: Task) => this.log(`added task w/ id=${newTask.id}`)),
+      catchError(this.handleError<Task>('addTask'))
+    );
+  }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.tasksUrl).pipe(
+      tap(_ => this.log('fetched tasks')),
+      catchError(this.handleError<Task[]>('getTasks', []))
+    );
   }
 
   getTask(id: number): Observable<Task> {
     const url = `${this.tasksUrl}/${id}`;
-    return this.http.get<Task>(url)
+    return this.http.get<Task>(url).pipe(
+      tap(_ => this.log(`fetched task id=${id}`)),
+      catchError(this.handleError<Task>(`getTask id=${id}`))
+    );
   }
 
-  deleteTask (task: Task | number): Observable<Task> {
+  updateTask(task: Task): Observable<any> {
+    return this.http.put(this.tasksUrl, task, this.httpOptions).pipe(
+      tap(_ => this.log(`updated task id=${task.id}`)),
+      catchError(this.handleError<any>('updateTask'))
+    );
+  }
+
+  deleteTask(task: Task | number): Observable<Task> {
     const id = typeof task === 'number' ? task : task.id;
     const url = `${this.tasksUrl}/${id}`;
-  
-    return this.http.delete<Task>(url, this.httpOptions)
+
+    return this.http.delete<Task>(url, this.httpOptions);
+  }
+
+  private log(message: string) {
+    this.messageSrv.add(`TaskService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
