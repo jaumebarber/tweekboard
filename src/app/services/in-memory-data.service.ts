a7942632737199611e6418ea4ef03f9d46ff673c
jebb <jebb@gft.com>
2019-08-27 16:04:02 +0200

    refactored columns component to be dynamically created an bound [not functional yet]

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -1,5 +1,4 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { Task } from '../models/task';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,18 +7,22 @@ import { Injectable } from '@angular/core';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const tasks = [
-      {id: 1, text: 'Get to work'},
-      {id: 2, text: 'Eat'},
-      {id: 3, text: 'Sleep'},
-      {id: 4, text: 'Dance'},
-      {id: 5, text: 'Shout'},
+    const columns = [
+      { id: 1,
+        title: 'ToDo',
+        tasks: [
+          {id: 1, text: 'Get to work'},
+          {id: 2, text: 'Eat'},
+          {id: 3, text: 'Sleep'},
+          {id: 4, text: 'Dance'},
+          {id: 5, text: 'Shout'}
+        ]
+      },
+      { id: 2, title: 'Doing', tasks: []},
+      { id: 3, title: 'Done', tasks: []},
     ];
-    return {tasks};
-  }
 
-  genId(tasks: Task[]): number {
-    return tasks.length > 0 ? Math.max(...tasks.map(task => task.id)) + 1 : 1;
+    return { columns };
   }
 }
 
