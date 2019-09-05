c3c2ec986de172c3a1eced66152b78688f20b76a
jaumebarber <jaumebarber@gmail.com>
2019-09-05 17:02:23 +0200

    [API] implemented API with translation service

---
+++ src/api/services/translations/ca.js
@@ -0,0 +1,22 @@
+class Catalan {
+    static retrieveTranslations () {
+      return {
+        appName: "Tweekboard",
+        boardTitle: "Què farem avui?",
+        cardOne: "Per fer",
+        cardTwo: "En curs",
+        cardThree: "Acabat",
+        addCard: "Afegiu una altra targeta",
+        removeCard: "Esborreu la targeta",
+        remCardConfirm: "N'esteu segurs?",
+        addList: "Afegiu una altra llista",
+        removeList: "Esborreu la llista",
+        remListConfirm: "N'esteu segurs?",
+        addBoard: "Afegiu un tauler",
+        removeBoard: "Esborreu el tauler",
+        remBoardConfirm: "N'esteu segurs d'esborrar aquest tauler? Tingueu en compte que tota la informació que conté serà esborrada permanentment.",
+      }
+    }
+  }
+  
+  module.exports = Catalan
---
+++ src/api/services/translations/en.js
@@ -0,0 +1,22 @@
+class English {
+  static retrieveTranslations () {
+    return {
+      appName: "Tweekboard",
+      boardTitle: "Focus for today",
+      cardOne: "ToDo",
+      cardTwo: "Doing",
+      cardThree: "Done",
+      addCard: "Add another card",
+      removeCard: "Remove this card",
+      remCardConfirm: "Sure to delete this card?",
+      addList: "Add a new list",
+      removeList: "Remove this list",
+      remListConfirm: "Sure to remove this list?",
+      addBoard: "Add a new board",
+      removeBoard: "Delete this board",
+      remBoardConfirm: "Sure to delete this board? Bear in mind that all the information contained will be erased permanently.",
+    }
+  }
+}
+
+module.exports = English
