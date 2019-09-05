f94f69a722c93dbf3dc9b50993892727d5271433
jaumebarber <jaumebarber@gmail.com>
2019-09-05 18:00:20 +0200

    [CSS] added buttons to navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
+import { AppComponent } from './app.component';
+import { ColumnsComponent } from './components/columns/columns.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { BoardComponent } from './components/board/board.component';
-import { CardComponent } from './components/card/card.component';
-
+import { MessagesComponent } from './components/messages/messages.component';
+import { BoardnavComponent } from './components/boardnav/boardnav.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ColumnsComponent,
     NavbarComponent,
-    BoardComponent,
-    CardComponent
+    MessagesComponent,
+    BoardnavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
+    LayoutModule,
     MatIconModule,
-    MatCardModule
+    MatListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatTooltipModule,
+    DragDropModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
